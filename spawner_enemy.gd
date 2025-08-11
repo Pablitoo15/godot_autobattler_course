7b6c77ba4c4e02bb7d8ebfe8b08104d205c40d13
Pawel <194279651+Pablitoo15@users.noreply.github.com>
2025-08-11 17:28:56 +0200

    changed size of units - to not block each other so much. change how units spawn

--- spawner_enemy.gd
+++ spawner_enemy.gd
@@ -27,8 +27,8 @@ func spawn_enemies():
 		parent_node.add_child(enemy)
 		enemy.global_position = sprite.global_position + i * Vector2(10, 0)
 	
-	enemies_to_spawn_min += 1
-	enemies_to_spawn_max += 1
+	if enemies_to_spawn_max > 1:
+		enemies_to_spawn_max += 1
 		
 func setup_timer():
 	timer_time = randf_range(wait_time_between_waves_min, wait_time_between_waves_max)
